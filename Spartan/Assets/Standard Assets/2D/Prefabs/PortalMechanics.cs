a1459e63f32da9d42874302ebe46d227803abc7e
eventowy <damianxwojcik@gmail.com>
2019-10-23 17:34:15 +0200

    UI small adjust + portals mechanics

---
+++ Spartan/Assets/Standard Assets/2D/Prefabs/PortalMechanics.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class PortalMechanics : MonoBehaviour
+{
+
+    public PlatformerCharacter2D Player;
+    public Transform DestinationPoint;
+    public Collider2D Trigger;
+
+
+    void Start()
+    {
+        Player = FindObjectOfType<PlatformerCharacter2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player.transform.position = DestinationPoint.position;
+
+            Trigger.enabled = false;
+            StartCoroutine("Wait");
+
+        }
+
+    }
+    public IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2f);
+        Trigger.enabled = true;
+    }
+}
