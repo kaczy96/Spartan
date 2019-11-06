cc4e773fcd798e9bbe05b420f60578528669b752
eventowy <damianxwojcik@gmail.com>
2019-11-06 13:44:53 +0100

    Small updates

---
+++ Spartan/Assets/SampleScenes/Scripts/Spikes.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Spikes : MonoBehaviour
+{
+    private Transform player;
+    public int dmg;
+    public bool hurting = false;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Damage();
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hurting = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hurting = false;
+        }
+    }
+
+    private void Update()
+    {
+
+        if (hurting)
+        {
+            Damage();
+        }
+    }
+
+    public void Damage()
+    {
+        Debug.Log("Enemy Attacking!");
+        //attackTime = attackCd;
+        player.SendMessageUpwards("DamagePlayer", dmg);
+    }
+}
+
