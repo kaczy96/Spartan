a7436dc00e3bfa999cbd2496458197a5b7fc9e7f
eventowy <damianxwojcik@gmail.com>
2019-11-19 22:26:19 +0100

    Enemy sprite rotates towards the player;

--- Spartan/Assets/Code/AIController.cs
+++ Spartan/Assets/Code/AIController.cs
@@ -4,44 +4,52 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
-    public float speed;
     private Transform target;
     public Enemy enemy;
+    public GameObject projectile;
+    private SpriteRenderer EnemySpriteRenderer;
+
+    public float speed;
     public float stoppingDistance;
     public float rangedDistance;
     public float retreatDistance;
     public float runningAwaySpeed;
     public float missileRange;
-
+    private bool m_FacingRight = true;
     private float timeBetweenShots;
     public float startTimeBetweenShots;
-    public GameObject projectile;
 
     private void Start()
     {
         enemy = GetComponent<Enemy>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         timeBetweenShots = startTimeBetweenShots;
+        EnemySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         if(Vector2.Distance(transform.position, target.position)>stoppingDistance)
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        
         else if (Vector2.Distance(transform.position, target.position) < stoppingDistance &&
                  Vector2.Distance(transform.position, target.position) > retreatDistance)
         {
             transform.position = this.transform.position;
         }
-        
         else if (Vector2.Distance(transform.position, target.position) < retreatDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, -runningAwaySpeed * Time.deltaTime);
         }
 
-        
-            
+        if (enemy.transform.position.x > target.transform.position.x && m_FacingRight)
+        {
+            Flip();
+        }
+        if (enemy.transform.position.x < target.transform.position.x && !m_FacingRight)
+        {
+            Flip();
+        }
+
         switch (enemy.enemyType)
         {
             case Enemy.EnemyType.Meele:
@@ -54,9 +62,16 @@ public class AIController : MonoBehaviour
                 Shoot();
                 break;
             case Enemy.EnemyType.Bug:
-                break;
-                
+                break;       
         }
+    }
+
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     void Shoot()
