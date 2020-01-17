d8f184220427a210c419102ce211764f2323483b
kris <kris@krej.net>
2020-01-17 14:59:13 +0100

    Titan v1

---
+++ Spartan/Assets/TitanController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TitanController : MonoBehaviour {
+    Player player;
+    Animator animator;
+
+    float cooldown;
+
+    public enum State {
+        Idle,
+        Attack
+    }
+
+    [SerializeField] State state = State.Idle;
+    [SerializeField] List<BossAttackZone> attackZones = new List<BossAttackZone>();
+    [SerializeField] BossAttackZone currentAttackZone;
+    
+    void Start() {
+        player = FindObjectOfType<Player>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        switch (state) {
+            case State.Idle:
+                if (IsPlayerInAttackRange() && IsReadyToAttack())
+                    ChangeStateToAttack();
+                break;
+            case State.Attack:
+                state = State.Idle;
+                animator.SetInteger("attackNr", 0);
+                break;
+        }
+
+        cooldown -= Time.deltaTime;
+    }
+
+    bool IsReadyToAttack() {
+        return cooldown <= 0;
+    }
+
+    void ChangeStateToAttack() {
+        state = State.Attack;
+        animator.SetInteger("attackNr", currentAttackZone.attackAnimationId);
+        cooldown = 0.4f + Random.Range(0f, 5f);
+    }
+
+    bool IsPlayerInAttackRange() {
+        ChooseAttackZone();
+        return currentAttackZone != null;
+    }
+
+    void ChooseAttackZone() {
+        foreach (var zone in attackZones) {
+            if (Physics2D.OverlapBoxAll(new Vector2(zone.collider.transform.position.x, zone.collider.transform.position.y), zone.collider.size, 0).Any(col => col.gameObject.tag == "Player")) {
+                currentAttackZone = zone;
+                return;
+            }
+        }
+        
+        currentAttackZone = null;
+    }
+}
+
+[System.Serializable]
+public class BossAttackZone {
+    public int attackAnimationId;
+    public BoxCollider2D collider;
+}
