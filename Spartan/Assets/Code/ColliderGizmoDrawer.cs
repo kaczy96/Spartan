0d59fe798d75165b5b0b730d087b19130fee0301
kris <kris@krej.net>
2020-01-24 13:23:27 +0100

    gizmos drawer

---
+++ Spartan/Assets/Code/ColliderGizmoDrawer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ColliderGizmoDrawer:MonoBehaviour {
+    BoxCollider2D collider;
+
+    void Awake() {
+        Init();
+    }
+
+    void Init() {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    public void OnDrawGizmos() {
+        if(collider == null)
+            Init();
+        var center = new Vector3(collider.transform.position.x + collider.offset.x,
+            collider.transform.position.y + collider.offset.y, collider.transform.position.z);
+        Gizmos.color = Color.magenta;
+        Debug.Log("!");
+        Gizmos.DrawWireCube(center, collider.size);
+    }
+}
